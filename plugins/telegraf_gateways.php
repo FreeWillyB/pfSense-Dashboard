3400734f13bdb476ab71b02cb29dc209dfbd016c
willblanton <jwblant@gmail.com>
2021-04-26 21:48:47 -0400

    Added extra functionality such as interface information and whether the gw is the default

--- plugins/telegraf_gateways.php
+++ plugins/telegraf_gateways.php
@@ -3,28 +3,60 @@
 
 require_once("gwlb.inc");
 
-$source = "pfconfig";
-$gwstat = return_gateways_status($true);
+$gw_array = return_gateways_array();
+$gw_statuses = return_gateways_status(true);
 
-foreach ($gwstat as $gw_ip => $gwaddress) {
-    $gateway = $gw_ip;
-    $monitor = $gwstat[$gw_ip]["monitorip"];
-    $source = $gwstat[$gw_ip]["srcip"];
-    $delay = $gwstat[$gw_ip]["delay"];
-    $stddev = $gwstat[$gw_ip]["stddev"];
-    $loss = $gwstat[$gw_ip]["loss"];
-    $status = $gwstat[$gw_ip]["status"];
-    $substatus = $gwstat[$gw_ip]["substatus"];
+foreach ($gw_array as $gw => $gateway) {
 
-    printf("gateways,gateway_name=%s monitor_ip=\"%s\",gateway_ip=\"%s\",rtt=%s,rttsd=%s,loss=%si,status=\"%s\",substatus=\"%s\"\n",
-        $gateway,
-        $monitor,
-        $source,
-        floatval($delay),
-        floatval($stddev),
+    $gw_name = $gw_statuses[$gw]["name"];
+    $monitor_ip = $gw_statuses[$gw]["monitorip"];
+    $gateway_ip = $gw_statuses[$gw]["srcip"];
+    $rtt = $gw_statuses[$gw]["delay"];
+    $rttsd = $gw_statuses[$gw]["stddev"];
+    $loss = $gw_statuses[$gw]["loss"];
+    $status = $gw_statuses[$gw]["status"];
+    
+    $interface = $gateway["interface"];
+    $interface_desc = $gateway["friendlyifdescr"];
+    $interface_friendly = $gateway["friendlyiface"]; // e.g. wan, lan, opt1, etc... 
+    $gwdescr = $gateway["descr"]; // NOT USED
+
+    
+    if (!isset($gateway['isdefaultgw'])) {
+        $defaultgw = "1";
+    } else {
+        $defaultgw = "0";
+    }
+        
+    if ($gw_statuses[$gw]) {
+        if (isset($gateway['monitor_disable'])) {
+            $monitor = "Unmonitored";
+            $delay = "Pending";
+            $stdev = "Pending";
+            $loss = "Pending";
+        }
+    }
+
+    // Some earlier versions of pfSense do not return substatus
+    if ($gw_statuses[$gw]["substatus"]) {
+        $substatus = $gw_statuses[$gw]["substatus"];
+    } else {
+        $substatus = "N/A";
+    }
+
+    printf("gateways,gateway_name=%s monitor_ip=\"%s\",gateway_ip=\"%s\",rtt=%s,rttsd=%s,loss=%si,status=\"%s\",substatus=\"%s\",interface=\"%s\",interface_desc=\"%s\",interface_friendly=\"%s\",default_gw=\"%s\"\n",
+        $gw_name,
+        $monitor_ip,
+        $gateway_ip,
+        floatval($rtt),
+        floatval($rttsd),
         floatval($loss),
         $status,
-        $substatus
+        $substatus,
+        $interface,
+        $interface_desc,
+        $interface_friendly,
+        $defaultgw
     );
 }
 ?>
